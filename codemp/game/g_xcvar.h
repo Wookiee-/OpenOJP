916c90198c5db89e3b0a47deba8e337478387ad9
Wookiee- <taggart_james@hotmail.com>
2026-07-06 14:48:50 +1000

    Fix TrueView defaults: disable invert, FOV override, and saber debug collision

--- codemp/game/g_xcvar.h
+++ codemp/game/g_xcvar.h
@@ -54,7 +54,7 @@ XCVAR_DEF( d_projectileGhoul2Collision,	"1",			NULL,						CVAR_CHEAT,										q
 XCVAR_DEF( d_saberAlwaysBoxTrace,		"0",			NULL,						CVAR_CHEAT,										qtrue )
 XCVAR_DEF( d_saberBoxTraceSize,			"0",			NULL,						CVAR_CHEAT,										qtrue )
 XCVAR_DEF( d_saberCombat,				"0",			NULL,						CVAR_CHEAT,										qfalse )
-XCVAR_DEF( d_saberGhoul2Collision,		"1",			NULL,						CVAR_CHEAT,										qtrue )
+XCVAR_DEF( d_saberGhoul2Collision,		"0",			NULL,						CVAR_CHEAT,										qtrue )
 XCVAR_DEF( d_saberInterpolate,			"0",			NULL,						CVAR_CHEAT,										qtrue )
 XCVAR_DEF( d_saberKickTweak,			"1",			NULL,						CVAR_NONE,										qtrue )
 XCVAR_DEF( d_saberSPStyleDamage,		"1",			NULL,						CVAR_ARCHIVE,									qfalse )
