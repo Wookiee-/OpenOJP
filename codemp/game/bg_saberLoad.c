ad351e1c506862cdf5fb49cfac9f95233526e750
Wookiee- <taggart_james@hotmail.com>
2026-07-06 17:00:35 +1000

    Fix TranslateSaberColor for white/black/rgb/pimp/scripted colors

--- codemp/game/bg_saberLoad.c
+++ codemp/game/bg_saberLoad.c
@@ -180,6 +180,16 @@ saber_colors_t TranslateSaberColor( const char *name ) {
 		return SABER_PURPLE;
 	if ( !Q_stricmp( name, "random" ) )
 		return (saber_colors_t)Q_irand( SABER_ORANGE, SABER_PURPLE );
+	if ( !Q_stricmp( name, "white" ) )
+		return (saber_colors_t)7;
+	if ( !Q_stricmp( name, "black" ) )
+		return (saber_colors_t)8;
+	if ( !Q_stricmp( name, "rgb" ) )
+		return (saber_colors_t)9;
+	if ( !Q_stricmp( name, "pimp" ) )
+		return (saber_colors_t)10;
+	if ( !Q_stricmp( name, "scripted" ) )
+		return (saber_colors_t)11;
 
 	return SABER_BLUE;
 }
@@ -191,6 +201,11 @@ const char *SaberColorToString( saber_colors_t color ) {
 	if ( color == SABER_GREEN )		return "green";
 	if ( color == SABER_BLUE )		return "blue";
 	if ( color == SABER_PURPLE )	return "purple";
+	if ( color == 7 )	return "white";
+	if ( color == 8 )	return "black";
+	if ( color == 9 )	return "rgb";
+	if ( color == 10 )	return "pimp";
+	if ( color == 11 )	return "scripted";
 
 	return NULL;
 }
